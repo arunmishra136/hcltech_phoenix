c76bf41040126388cacb44e6dc75dcb4677a1cc1
bestcoder22 <amrutkarsoham22@gmail.com>
2025-12-19 15:01:23 +0530

    update 1

---
+++ backend/model/patient.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+const patientSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true, lowercase: true },
+  password: { type: String, required: true },
+  age: Number,
+  allergies: [String],     // array of strings
+  sleepTime: Number,
+  exerciseTime: Number
+}, { timestamps: true });
+
+export default mongoose.model("patient", patientSchema);
