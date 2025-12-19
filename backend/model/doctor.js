3708e9131a1fb2069c9462418493f4e779f1b286
arunmishra136 <mishraarun136@gmail.com>
2025-12-19 14:53:58 +0530

    others

---
+++ backend/model/doctor.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+const doctorSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true, lowercase: true },
+  password: { type: String, required: true },
+  specialization: String,
+  experience: Number,
+  rating: Number,
+  availability: String        
+}, { timestamps: true });
+
+export default mongoose.model("doctor", doctorSchema);
